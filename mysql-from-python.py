55a2c7a6f3ba3d280aeb265e3a8f34d4505c2919
dudeguythethird <keablejoseph@gmail.com>
2021-02-08 12:00:08 +0000

    removed sql file

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -4,13 +4,13 @@ import pymysql
 connection = pymysql.connect(host='localhost',
                             db='Chinook')
 
-try: 
-    #Run a query
+try:
+    # Run a query
     with connection.cursor() as cursor:
         sql = "SELECT * FROM Artist;"
         cursor.execute(sql)
         result = cursor.fetchall()
         print(result)
 finally:
-    #Close the connection, regardless of whether the above was succesful
+    # Close the connection, regardless of whether the above was succesful
     connection.close()
